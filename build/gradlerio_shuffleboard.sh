12ec76a4391dc09b4db89c7465af24ece33f6ff4
jscd <desimpliciis@gmail.com>
2018-01-20 13:13:39 -0500

    Add gradlew using GradleRIO

---
+++ build/gradlerio_shuffleboard.sh
@@ -0,0 +1,3 @@
+pushd /Users/lunyrobot/.gradle/gradlerio/Shuffleboard
+/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/bin/java "-jar" "/Users/lunyrobot/.gradle/caches/modules-2/files-2.1/edu.wpi.first.shuffleboard/app/1.0.0/adc0b59e08073c0ff9c352e282118bae68e10a2a/app-1.0.0.jar"
+popd
---
+++ build/gradlerio_smartDashboard.sh
@@ -0,0 +1,3 @@
+pushd /Users/lunyrobot/.gradle/gradlerio/SmartDashboard
+/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/bin/java "-jar" "/Users/lunyrobot/.gradle/caches/modules-2/files-2.1/edu.wpi.first.wpilib/SmartDashboard/2.0.4/e57abbf4dea1b63c6c5dcaf7c19b4972bcbd09ff/SmartDashboard-2.0.4.jar"
+popd
