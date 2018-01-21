add66e6287c91a9690d35b3feab504e83c6d7b5d
jscd <desimpliciis@gmail.com>
2018-01-21 14:06:50 -0500

    Emphasize temporary values in Definitions.h

--- src/include/Definitions.h
+++ src/include/Definitions.h
@@ -1,12 +1,14 @@
 #ifndef DEFINITIONS_H_
 #define DEFINITIONS_H_
 
-#define CL_SPEED            1
+#define TBD                 0
 
-// *TO-DO: TEST FOR PROPER VALUES
-#define SHIFT_UP_SPEED      0
-#define SHIFT_DOWN_SPEED    0
-#define CL_SHIFT_UP_SPEED   0
-#define CL_SHIFT_DOWN_SPEED 0
+// *TO-DO: CHANGE TBDs to actual values
+#define CL_SPEED            TBD
+
+#define SHIFT_UP_SPEED      TBD
+#define SHIFT_DOWN_SPEED    TBD
+#define CL_SHIFT_UP_SPEED   TBD
+#define CL_SHIFT_DOWN_SPEED TBD
 
 #endif // DEFINITIONS_H_
