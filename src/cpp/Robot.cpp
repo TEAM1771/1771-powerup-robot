1a15aa9cd08f28bd32dc4f873f6c337157f2afef
jscd <desimpliciis@gmail.com>
2018-01-21 14:42:16 -0500

    Implement DriveTrain class into Robot.cpp

--- src/cpp/Robot.cpp
+++ src/cpp/Robot.cpp
@@ -7,12 +7,21 @@
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 
+/* Our classes */
+#include <Definitions.h>
+#include <DriveTrain.h>
 
 using namespace frc;
 
 class Robot: public IterativeRobot {
+private:
+    DriveTrain driveTrain;
 public:
-
+    
+    Robot() :  driveTrain(LTR_MOTOR_A, LTR_MOTOR_B, RTR_MOTOR_A, RTR_MOTOR_B, L_ENC_CHA, L_ENC_CHB, R_ENC_CHA, R_ENC_CHB, SHIFTER_PORT, PTO_PORT){
+        
+    }
+    
     void RobotInit() { }
 
     void DisabledInit() { }
@@ -22,7 +31,13 @@ public:
 
     void DisabledPeriodic() { }
     void AutonomousPeriodic() { }
-    void TeleopPeriodic() { }
+    
+    void TeleopPeriodic() {
+        // *TO-DO: Change to joystick val functions
+        driveTrain.Tank(TBD, TBD);
+        driveTrain.AutoShift();
+    }
+    
     void TestPeriodic() { }
 };
 
