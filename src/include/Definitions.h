361b851f02be546d6ed79715237949cbfe002575
jscd <desimpliciis@gmail.com>
2018-01-20 19:49:34 -0500

    Add Definitions header file

---
+++ src/include/Definitions.h
@@ -0,0 +1,12 @@
+#ifndef DEFINITIONS_H_
+#define DEFINITIONS_H_
+
+#define CL_SPEED            1
+
+// *TO-DO: TEST FOR PROPER VALUES
+#define SHIFT_UP_SPEED      0
+#define SHIFT_DOWN_SPEED    0
+#define CL_SHIFT_UP_SPEED   0
+#define CL_SHIFT_DOWN_SPEED 0
+
+#endif // DEFINITIONS_H_
