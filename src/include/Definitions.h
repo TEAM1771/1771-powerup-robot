75e0c0171c2ce338ba7c148aa0811083655766b6
jscd <desimpliciis@gmail.com>
2018-03-31 07:20:13 -0400

    Add Encoder constants and change button for ease of use

--- src/include/Definitions.h
+++ src/include/Definitions.h
@@ -7,15 +7,18 @@
 
 #define PI                  3.1415926535897932384623383279
 #define sigmoid(x)			1.0/(1+exp(x))
+#define RIO_CYCLE           .02
 
 /*========== Button Mappings ==========*/
-#define CLIMB_BUTTON_A      7
-#define CLIMB_BUTTON_B      10
-#define INTAKE_BUTTON_IN    3
-#define INTAKE_BUTTON_SHOOT 4
-#define INTAKE_BUTTON_IN_PT 5
-#define INTAKE_BUTTON_ANGLE	6
-#define MANUAL_SHIFT_BUTTON 11
+#define INTAKE_POS_BUTTON   in.GetOtherButton(5)
+#define SHOOT_POS_BUTTON    in.GetOtherButton(6)
+#define INTAKE_IN_BUTTON    in.GetOtherButton(3)
+#define INTAKE_OUT_BUTTON   in.GetOtherButton(4)
+#define INTAKE_FLAP_BUTTON  in.GetOtherButton(1)
+#define ELVTR_SCALE_BUTTON  in.GetOtherButton(7)
+#define ELVTR_DOWN_BUTTON   in.GetOtherButton(11)
+#define ELVTR_SWITCH_BUTTON in.GetOtherButton(9)
+
 
 /*========== Ports and Encoder channels ==========*/
 #define JOY_LEFT            0
@@ -25,27 +28,27 @@
 // Motors
 #define LTR_MOTOR_A         4
 #define LTR_MOTOR_B         2
-#define RTR_MOTOR_A         3
-#define RTR_MOTOR_B         7
-#define ELEVATOR_MOTOR_PORT TBD
-#define RIGHT_ARM_IN_PORT   9
-#define LEFT_ARM_IN_PORT    5
-#define ARM_PIVOT_PORT      1
+#define RTR_MOTOR_A         7
+#define RTR_MOTOR_B         10
+#define ELEVATOR_MOTOR_PORT 1
+#define RIGHT_ARM_IN_PORT   3
+#define LEFT_ARM_IN_PORT    6
+#define ARM_PIVOT_PORT      7
 
 // Encoders (and a single potentiometer)
-#define L_ENC_CHA           TBD
-#define L_ENC_CHB           TBD
-#define R_ENC_CHA           TBD
-#define R_ENC_CHB           TBD
+#define L_ENC_CHA           0
+#define L_ENC_CHB           1
+#define R_ENC_CHA           2
+#define R_ENC_CHB           3
 #define ELEVATOR_ENC_CHA    TBD
 #define ELEVATOR_ENC_CHB    TBD
 #define ARM_PIVOT_ENC_A	    4
 #define ARM_PIVOT_ENC_B     5
 
 // Solenoids
-#define SHIFTER_PORT        TBD
+#define SHIFTER_PORT        0
 #define PTO_PORT            TBD
-#define FLIPPER_PORT        0
+#define FLIPPER_PORT        1
 #define WINGS_PORT          TBD
 
 /*========== Speed constants and Encoder thresholds ==========*/
@@ -55,23 +58,24 @@
 #define SHIFT_DOWN_SPEED    2200
 #define CL_SHIFT_UP_SPEED   TBD
 #define CL_SHIFT_DOWN_SPEED TBD
-#define ELEVATOR_HIGH_PT    TBD
-#define ELEVATOR_LOW_PT     TBD
+#define ELEVATOR_SCALE_PT   38460.0
+#define ELEVATOR_DOWN_PT    0.0
+#define ELEVATOR_SWITCH_PT  10644.0
 #define INTAKE_WHEEL_SPEED  1
-#define INTAKE_IN_PT        -5038.0
-#define INTAKE_UP_PT        0.0
-#define INTAKE_SHOOT_PT     -2016.0
+#define INTAKE_IN_PT        -4469.0
+#define INTAKE_UP_PT        -1000
+#define INTAKE_SHOOT_PT     TBD
 
 #define ARM_PIVOT_SPEED     TBD
 
 /*========== Coefficients for PID controllers ==========*/
-#define ELEVATOR_P          TBD
-#define ELEVATOR_I          TBD
-#define ELEVATOR_D          TBD
+#define ELEVATOR_P          2
+#define ELEVATOR_I          0
+#define ELEVATOR_D          0
+#define INTAKE_P			1.4
 
 /*========== Tipping Angle Constants ==========*/
-#define FORWARD_TIP         18
-#define BACKWARD_TIP        -18
+#define TIP_RATE_MAX        18
 
 
 #endif // DEFINITIONS_H_
