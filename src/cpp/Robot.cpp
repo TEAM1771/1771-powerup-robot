d1604284fc6ad3444db8f7459515ac3ac07e95c7
jscd <desimpliciis@gmail.com>
2018-03-31 07:21:54 -0400

    Organize main program for easier code handling, add elevator position handling

--- src/cpp/Robot.cpp
+++ src/cpp/Robot.cpp
@@ -16,8 +16,6 @@
 #include <Point.h>
 
 
-using namespace frc;
-
 class Robot: public IterativeRobot {
 private:
     frc::LiveWindow* lw = LiveWindow::GetInstance();
@@ -35,11 +33,16 @@ private:
     bool naverr = 0;
     double lastdist = 0;
     double start_angle;
+    double navlastroll = 0.0;
+    double dRoll = 0.0;
     
     // Point for auton
     Point currentPoint;
     
     int intake_val = 0;
+    
+    enum ELEVATOR_POSITIONS { ELEVATOR_DOWN, ELEVATOR_SWITCH, ELEVATOR_SCALE };
+    int elevatorPosition = ELEVATOR_DOWN;
     
 public:
     
@@ -68,44 +71,41 @@ public:
         navpitchinit = navx->GetPitch();
     }
 
-    void DisabledInit() { }
+    void DisabledInit() {
+		elevator.SetNeutralMode(NeutralMode::Brake);
+	}
     
-    void AutonomousInit() {
-        navx->ZeroYaw();
-        start_angle = navx->GetAngle();
-    }
+    void AutonomousInit() {}
+    
     void TeleopInit() {
         navrollinit = navx->GetRoll();
         navpitchinit = navx->GetPitch();
         elevator.intake.SetFlipper(1);
+		elevatorPosition = ELEVATOR_DOWN_PT;
     }
     
-    void TestInit() { }
+    void TestInit() {
+        navrollinit = navx->GetRoll();
+        navpitchinit = navx->GetPitch();
+        elevator.intake.SetFlipper(1);
+    }
 
     void DisabledPeriodic() {
         PutNumbers();
     }
+    
     void AutonomousPeriodic() {
-//        Point p;
-//        double avgds = driveTrain.GetAvgRaw() - lastdist;
-//        p.Set(avgds*cos(navx->GetAngle()*PI/180.0), avgds*sin(navx->GetAngle()*PI/180.0));
-//        currentPoint += p;
-//        lastdist = driveTrain.GetAvgRaw();
+        Point p;
+        double avgds = driveTrain.GetAvgRaw() - lastdist;
+        p.Set(avgds*cos(navx->GetAngle()*PI/180.0), avgds*sin(navx->GetAngle()*PI/180.0));
+        currentPoint += p;
+        lastdist = driveTrain.GetAvgRaw();
         
-        if(driveTrain.GetAvgRaw() < 7000){
-            
-            double left_mod = 1.0, right_mod = 1.0;
-            
-            if(navx->GetAngle() > start_angle + 1){
-                left_mod = 1.7;
-            } if(navx->GetAngle() < start_angle - 1){
-                right_mod = 1.7;
-            }
-            
-            driveTrain.Tank(0.6 * left_mod, 0.6 * right_mod);
-            
-            
+        if(Timer::GetMatchTime() <= 9){
+            driveTrain.Tank(-0.2, -0.2);
         }
+        
+        AntiTip();
     }
     
     void TeleopPeriodic() {
@@ -114,55 +114,84 @@ public:
          */
         
         PutNumbers();
-//        if(in.GetLeftButton(CLIMB_BUTTON_A) && in.GetLeftButton(CLIMB_BUTTON_B)){
-//            driveTrain.EnableClimb();
-//        }
         
         if(!driveTrain.IsClimbing()){
-//            if(navx->GetRoll()-navrollinit > FORWARD_TIP){
-//                driveTrain.Tank(1.5 * in.GetLeftY(), 1.5 * in.GetRightY());
-//            }else if(navx->GetRoll()-navrollinit < BACKWARD_TIP){
-//                driveTrain.Tank(-1.5 * in.GetLeftY(), -1.5 * in.GetRightY());
-//            }else{ // Bot is not tipping
-                driveTrain.Tank(in.GetLeftY(),in.GetRightY());
-            //          }
             
-            intake_val = 0;
+            driveTrain.Tank(in.GetLeftY(),in.GetRightY());
             
-            if(in.GetLeftButton(INTAKE_BUTTON_IN)){
-                elevator.intake.SetPivotArm(INTAKE_IN_PT);
-                elevator.intake.SetIntakeWheels(-INTAKE_WHEEL_SPEED);
-            }else if(in.GetLeftButton(INTAKE_BUTTON_SHOOT)){
-                if(elevator.intake.SetPivotArm(INTAKE_SHOOT_PT));
-                    elevator.intake.SetIntakeWheels(INTAKE_WHEEL_SPEED);
-            }else{
-                elevator.intake.SetPivotArm(INTAKE_UP_PT);
-                elevator.intake.SetIntakeWheels(-INTAKE_WHEEL_SPEED * 0.2);
-            }
+            IntakePosition();
+            IntakeWheelControl();
             
-            if(in.GetLeftButton(MANUAL_SHIFT_BUTTON)){
-                driveTrain.Shift(1);
-            }else{
-                driveTrain.Shift(0);
-            }
-            //driveTrain.AutoShift();
+			
+            ControlElevator();
+            elevator.UpdatePID();
+            elevator.SetPosition(elevatorPosition);
+			
+            // Sarah is the best :3
+            driveTrain.AutoShift();
         }else{
             driveTrain.Tank(in.GetLeftY(), in.GetRightY());
             /** CLIMB SHIFTING MIGHT BREAK EVERYTHING BEWARE **/
-            driveTrain.AutoShift();
+            // driveTrain.AutoShift();
         }
-        
+    }
+    
+    void TestPeriodic() {
+        PutNumbers();
+    }
+    
+    
+    /** User-Defined Functions **/
+    
+    void ControlElevator(){
+        if(ELVTR_DOWN_BUTTON){
+            elevatorPosition = ELEVATOR_DOWN_PT;
+        }else if(ELVTR_SCALE_BUTTON){
+            elevatorPosition = ELEVATOR_SCALE_PT;
+        }else if(ELVTR_SWITCH_BUTTON){
+            elevatorPosition = ELEVATOR_SWITCH_PT;
+        }
+    }
+    
+    
+    
+    // Intake Positioning
+    void IntakePosition(){
+        if(INTAKE_POS_BUTTON){
+			elevator.intake.SetPivotArm(INTAKE_IN_PT);
+		}else{
+			elevator.intake.SetPivotArm(INTAKE_UP_PT);
+		}
+    }
+    
+    // Intake Wheel Control
+    void IntakeWheelControl(){
+        if(INTAKE_IN_BUTTON){
+            elevator.intake.SetIntakeWheels(-INTAKE_WHEEL_SPEED);
+        }else if(INTAKE_OUT_BUTTON){
+            elevator.intake.SetIntakeWheels(INTAKE_WHEEL_SPEED);
+        }else{
+            elevator.intake.SetIntakeWheels(INTAKE_WHEEL_SPEED * 0.15);
+        }
+    }
+    
+    // AntiTip -> Uses the derivative of the angle (the rate of change in angle) to engage motors to prevent tipping
+    void AntiTip(){
+        /* DO NOT REMOVE INTAKE UP ANTI-TIP */
+        dRoll = (navlastroll - (navx->GetRoll()-navrollinit))/RIO_CYCLE;
+        navrollinit = navx->GetRoll();
+        if(fabs(dRoll) > TIP_RATE_MAX){
+            elevator.intake.SetPivotArm(INTAKE_UP_PT);
+        }
         
     }
     
-    void TestPeriodic() { }
     
     void PutNumbers(){
         SmartDashboard::PutString("Climb Status: ", std::to_string(driveTrain.IsClimbing()));
         SmartDashboard::PutString("NavX Angle: ",std::to_string(navx->GetAngle()));
         SmartDashboard::PutString("NavX Pitch: ", std::to_string(navx->GetRoll()-navrollinit));
         SmartDashboard::PutString("NavX Pitch Raw: ", std::to_string(navx->GetRoll()));
-        SmartDashboard::PutString("Anti-tip: ", std::to_string((navx->GetRoll()-navrollinit > FORWARD_TIP || navx->GetRoll()-navrollinit < BACKWARD_TIP)));
         SmartDashboard::PutString("NavX Error Status: ",std::to_string(naverr));
         SmartDashboard::PutString("Left Joystick Y: ", std::to_string(in.GetLeftY()));
         SmartDashboard::PutString("Right Joystick Y: ", std::to_string(in.GetRightY()));
@@ -171,6 +200,9 @@ public:
         SmartDashboard::PutString("Y:", std::to_string(currentPoint.GetY()));
         SmartDashboard::PutString("Hall Effect Status:", std::to_string(hall_effect_sensor.Get()));
         SmartDashboard::PutString("Intake Wheel Direction:", std::to_string(intake_val));
+        SmartDashboard::PutString("Pivot Arm Enc:", std::to_string(elevator.intake.GetPivotEnc()));
+        SmartDashboard::PutString("dRoll:", std::to_string(dRoll));
+        SmartDashboard::PutString("Elevator Encoder Value:", std::to_string(elevator.GetElvtrEnc()));
     }
 };
 
