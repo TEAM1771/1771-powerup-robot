75de8d2d1cb0c37eb2563e5261a8b5a9872ef16d
jscd <desimpliciis@gmail.com>
2018-03-31 07:18:07 -0400

    Add functions for talon encoders

--- src/include/Elevator.h
+++ src/include/Elevator.h
@@ -13,25 +13,29 @@
 class Elevator {
 private:
     TalonSRX elvtr;
-    Encoder elvtr_enc;
+    
     
     // Elevator PI control
     double change;
     int error, desired_pos, derivative, last_err;
     long int i;
     
+	bool arm_mode;
     bool reaching_pos;
 public:
     Intake intake;
+    Encoder elvtr_enc;
     
     Elevator(int elv_port, int elv_ch_a, int elv_ch_b, int arm_pivot_pot_port, int arm_pivot_enc_a, int arm_pivot_enc_b, int flipper_port, int rin_port, int lin_port);
     
     void SetPosition(int position);
+	void SetNeutralMode(NeutralMode nm);
     
     void UpdatePID();
     
     void Set(double rate);
     void SetForJoy(double rate);
+	double GetElvtrEnc();
 };
 
 #endif // ELEVATOR_H_
