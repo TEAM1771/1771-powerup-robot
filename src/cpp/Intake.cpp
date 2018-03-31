83cabd1b67b624dd81e956b1ce452d0a75de9c12
jscd <desimpliciis@gmail.com>
2018-03-31 07:19:22 -0400

    Rewrite Intake pivot arm code to support positive and negative encoder values

--- src/cpp/Intake.cpp
+++ src/cpp/Intake.cpp
@@ -6,6 +6,7 @@ flipper(flipper_port),
 lin(lin_port), rin(rin_port){
 	arm_pivot.ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, 0);
 	arm_pivot.SetSensorPhase(0);
+	arm_pivot.SetNeutralMode(NeutralMode::Brake);
 }
 
 void Intake::ZeroEnc(){
@@ -25,20 +26,16 @@ void Intake::SetFlipper(bool flip){
 int Intake::SetPivotArm(double pos){
 	
     SmartDashboard::PutString("EncVal:", std::to_string(arm_pivot.GetSelectedSensorPosition(0)));
-	double speed = fabs((pos-GetPivotEnc())/INTAKE_IN_PT);
-    if(GetPivotEnc() > pos-25){
-    	arm_pivot.Set(ControlMode::PercentOutput, -speed);
-	return 0;
-    }else if(arm_pivot.GetSelectedSensorPosition(0) < pos+25){
-        arm_pivot.Set(ControlMode::PercentOutput, speed);
-        return 0;
-    }else{
-        return 1;
-    }
+	double speed = INTAKE_P*fabs((pos-GetPivotEnc())/fabs(INTAKE_IN_PT));
+	if(fabs(pos) - fabs(GetPivotEnc()) > 10){
+		SetPivotSpeed(-speed);
+	}else if(fabs(pos) - fabs(GetPivotEnc()) < -10){
+		SetPivotSpeed(speed);
+	}
 }
 
 void Intake::SetPivotSpeed(double speed){
-	if(!(GetPivotEnc()-20 < INTAKE_UP_PT) && !(GetPivotEnc()+20 > INTAKE_IN_PT))
+	//if(!(GetPivotEnc()-20 < INTAKE_UP_PT) && !(GetPivotEnc()+20 > INTAKE_IN_PT))
 	arm_pivot.Set(ControlMode::PercentOutput, speed);
 }
 
